33b25b2a6a6a2bc0ca0b4ad13fd5894e1a36f1bf
David Kahler <david.m.kahler@gmail.com>
2022-05-23 11:34:27 -0400

    added git pull to avoid problems with out-of-sync repo

--- medike.sh
+++ medike.sh
@@ -4,7 +4,10 @@ read mrid < "medike_mrid_next.txt"
 
 curl -X GET "https://zentracloud.com/api/v3/get_readings/?device_sn=z6-03791&start_mrid="$mrid"&output_format=csv&per_page=2000&sort_by=ascending" -H  "accept: application/json" -H  "Authorization: Token b8f6a7828d457419b93b8b2d2a6679ea2ddf012b" > apidownload.csv
 
+git pull
+
 Rscript sort.R "medike"
+
 git add medike_mrid.csv
 git add medike_mrid_next.txt
 git commit -m "updated logs - Medike"
